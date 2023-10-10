c4e9406c9b29302ac224ff56b3af44bec4727821
parkseongjun2298 <70846907+parkseongjun2298@users.noreply.github.com>
2023-10-10 17:20:13 +0900

    1010 staticmesh생성 tick log

---
+++ Source/Portpolio01/Portpolio01GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Portpolio01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO01_API APortpolio01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
